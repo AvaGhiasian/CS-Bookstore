54437a155890bcc85e9649efe7f849c03c842766
Ava Ghiasian <avaghiasian82@gmail.com>
2025-07-28 16:05:47 +0330

    wish i dont ruin everything

--- cs_bookstore/settings.py
+++ cs_bookstore/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zweis1h+@8o_1c@7i+wbc&m16mx(t-d0z*m1peh(ie(!^@58eq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.elasticbeanstalk.com']
 
 # Application definition
 
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
